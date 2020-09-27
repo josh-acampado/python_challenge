bfe6fdaf52abb71196e0f7f09f055a03772ff7dd
Josh Acampado <josh.acampado@gmail.com>
2020-09-27 17:10:58 -0400

    PyBank unable to loop after print

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,23 +8,14 @@ csvpath = os.path.join('Resources','budget_data.csv')
 #open the csv
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
+   
     #set header row
     csvheader = next(csvfile)
-
+   
     #Count total number of months
     line = len(list(csvreader))
-
-    #Define starting variable
-    profit_sum = 0
-    #Sum of Profit/Losses
-    
-    
-    #Mean of Profit/Losses, sum/total months
-
-    #Max of Profit/Losses from month to month
-
-    #Min of Profit/Losses from month to month
-    
-    #Print final results
     print ("Total Number of Months: " + str(line))
-    print("Total: $" + str(profit_sum))
+
+    #Total of Profit/Losses
+    for row in csvreader:
+        print (row)
