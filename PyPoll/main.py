644669866e57472e0f1e396341040e16dbd4a7d3
Josh Acampado <josh.acampado@gmail.com>
2020-09-28 16:11:58 -0400

    Finished printing results to terminal

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -42,10 +42,34 @@ with open(csvpath, 'r') as csvfile:
         if row[2] == "O'Tooley":
             otooley_counter = otooley_counter + 1
     
-    #calculate after loop
+    #calculate after loop, calculate percent, add value to empty list
     khan_percent = round(((khan_counter / total_vote_counter) * 100), 3)
+    candidates_percentages.append(khan_percent)
+    
     correy_percent = round(((correy_counter / total_vote_counter) * 100), 3)
+    candidates_percentages.append(correy_percent)
+   
     li_percent = round(((li_counter / total_vote_counter) * 100), 3)
+    candidates_percentages.append(li_percent)
+   
     otooley_percent = round(((otooley_counter / total_vote_counter) * 100), 3)
+    candidates_percentages.append(otooley_percent)
 
+    #Identify max value (max percent) to identify the winner
+    percent_max = max(candidates_percentages)
+    #Identify the max value index
+    percent_max_index = candidates_percentages.index(percent_max)
+    #Identify winning candidate by finding matching candidate to index
+    winner = candidates[percent_max_index]
+
+print("Election Results")
+print("-------------------------------")
 print("Total Votes: " + str(total_vote_counter))
+print("-------------------------------")
+print(str(candidates[0]) + ": " + str(khan_percent) + "% " + "(" + str(khan_counter) + ")")
+print(str(candidates[1]) + ": " + str(correy_percent) + "% " + "(" + str(correy_counter) + ")")
+print(str(candidates[2]) + ": " + str(li_percent) + "% " + "(" + str(li_counter) + ")")
+print(str(candidates[3]) + ": " + str(otooley_percent) + "% " + "(" + str(otooley_counter) + ")")
+print("-------------------------------")
+print("Winner: " + str(winner))
+print("-------------------------------")
